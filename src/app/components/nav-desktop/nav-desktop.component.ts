bd5b015367d859175468b6d3ae8b38edb337a2f8
Karol Ciak <ciakkarol@hotmail.com>
2023-08-23 18:56:53 +0200

    done navDesktop

--- src/app/components/nav-desktop/nav-desktop.component.ts
+++ src/app/components/nav-desktop/nav-desktop.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-desktop',
   templateUrl: './nav-desktop.component.html',
-  styleUrls: ['./nav-desktop.component.scss']
+  styleUrls: ['./nav-desktop.component.scss'],
 })
-export class NavDesktopComponent {
+export class NavDesktopComponent implements OnInit {
+  listElementBG: string = '';
+  listElementText: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.checkCurrentRoute();
+  }
+
+  checkCurrentRoute() {
+    this.listElementBG = this.router.url;
+    this.listElementText = this.router.url;
+  }
 }
