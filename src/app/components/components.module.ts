09c994947d16393fd6311dc5dd06717d4b1de665
Karol Ciak <ciakkarol@hotmail.com>
2023-08-27 22:43:36 +0200

    modify navDesktop & aboutme / add footer

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,14 +4,21 @@ import { NavMobileComponent } from './navMobile/navMobile.component';
 import { RouterModule } from '@angular/router';
 import { NavDesktopComponent } from './nav-desktop/nav-desktop.component';
 import { PresentationPageComponent } from './presentation-page/presentation-page.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     NavMobileComponent,
     NavDesktopComponent,
     PresentationPageComponent,
+    FooterComponent,
   ],
   imports: [CommonModule, RouterModule],
-  exports: [NavMobileComponent, NavDesktopComponent, PresentationPageComponent],
+  exports: [
+    NavMobileComponent,
+    NavDesktopComponent,
+    PresentationPageComponent,
+    FooterComponent,
+  ],
 })
 export class ComponentsModule {}
