9ff12853f2bb93494833ceedad3da0ebc8c5d1d6
Karol Ciak <ciakkarol@hotmail.com>
2023-09-03 21:55:48 +0200

    add animations to myskills page

---
+++ src/app/models/my-skills.ts
@@ -0,0 +1,9 @@
+export interface mainIconsModel {
+  url: string;
+  name: string;
+}
+
+export interface otherIconsModel {
+  url: string;
+  name: string;
+}
--- src/app/pages/myskills/my-skills.component.ts
+++ src/app/pages/myskills/my-skills.component.ts
@@ -1,33 +1,74 @@
 import { Component } from '@angular/core';
+import { flipAnimation, pulseAnimation } from 'angular-animations';
+import { mainIconsModel, otherIconsModel } from 'src/app/models/my-skills';
 
 @Component({
   selector: 'app-my-skills',
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss'],
+  animations: [
+    flipAnimation({ duration: 1500 }),
+    pulseAnimation({ scale: 1.3 }),
+  ],
 })
 export class MySkillsComponent {
-  mainIcons: string[] = [
-    '../../../assets/icons/java.png',
-    '../../../assets/icons/angular.png',
+  mainIcons: mainIconsModel[] = [
+    { url: '../../../assets/icons/java.png', name: 'java' },
+    { url: '../../../assets/icons/angular.png', name: 'angular' },
   ];
 
-  otherIcons: string[] = [
-    '../../../assets/icons/bash.png',
-    '../../../assets/icons/bootstrap.svg',
-    '../../../assets/icons/css.png',
-    '../../../assets/icons/git.png',
-    '../../../assets/icons/github.png',
-    '../../../assets/icons/hibernate.png',
-    '../../../assets/icons/html.png',
-    '../../../assets/icons/intellij.png',
-    '../../../assets/icons/js.png',
-    '../../../assets/icons/mysql.png',
-    '../../../assets/icons/node.png',
-    '../../../assets/icons/posgre.png',
-    '../../../assets/icons/postman.png',
-    '../../../assets/icons/sass.png',
-    '../../../assets/icons/spring.png',
-    '../../../assets/icons/thymeleaf.png',
-    '../../../assets/icons/typescript.png',
+  otherIcons: otherIconsModel[] = [
+    { url: '../../../assets/icons/bash.png', name: 'bash' },
+    { url: '../../../assets/icons/bootstrap.svg', name: 'bootstrap' },
+    { url: '../../../assets/icons/css.png', name: 'css' },
+    { url: '../../../assets/icons/git.png', name: 'git' },
+    { url: '../../../assets/icons/github.png', name: 'github' },
+    { url: '../../../assets/icons/hibernate.png', name: 'hibernate' },
+    { url: '../../../assets/icons/html.png', name: 'html' },
+    { url: '../../../assets/icons/intellij.png', name: 'intellij' },
+    { url: '../../../assets/icons/js.png', name: 'JavaScript' },
+    { url: '../../../assets/icons/mysql.png', name: 'mysql' },
+    { url: '../../../assets/icons/node.png', name: 'node' },
+    { url: '../../../assets/icons/posgre.png', name: 'posgreSQL' },
+    { url: '../../../assets/icons/postman.png', name: 'postman' },
+    { url: '../../../assets/icons/sass.png', name: 'sass' },
+    { url: '../../../assets/icons/spring.png', name: 'spring' },
+    { url: '../../../assets/icons/thymeleaf.png', name: 'thymeleaf' },
+    { url: '../../../assets/icons/typescript.png', name: 'typescript' },
   ];
+
+  mainIconsState: { [key: string]: boolean } = {
+    java: false,
+    angular: false,
+  };
+
+  otherIconsState: { [key: string]: boolean } = {
+    bash: false,
+    bootstrap: false,
+    css: false,
+    git: false,
+    github: false,
+    hibernate: false,
+    html: false,
+    intellij: false,
+    js: false,
+    mysql: false,
+    node: false,
+    posgre: false,
+    postman: false,
+    sass: false,
+    spring: false,
+    thymeleaf: false,
+    typescript: false,
+  };
+
+  otherIconsChangeState(nameOfIcon: string): void {
+    this.otherIconsState[nameOfIcon] = true;
+    setTimeout(() => (this.otherIconsState[nameOfIcon] = false), 1);
+  }
+
+  mainIconsChangeState(nameOfIcon: string): void {
+    this.mainIconsState[nameOfIcon] = true;
+    setTimeout(() => (this.mainIconsState[nameOfIcon] = false), 1);
+  }
 }
