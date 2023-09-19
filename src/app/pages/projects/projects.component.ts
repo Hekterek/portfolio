d8d2261c6668ac979bafad5f3e9071dfd7ff1e51
Karol Ciak <ciakkarol@hotmail.com>
2023-09-19 10:40:51 +0200

    edit project page

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-
+  projects: any = [
+    {
+      name: 'Finance app',
+      desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+      Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+      when an unknown printer took a galley of type and scrambled it to make a type 
+      specimen book. It has survived not only five centuries, but also the leap into 
+      electronic typesetting, remaining essentially unchanged. It was popularised in 
+      the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+      and more recently with desktop publishing software like Aldus PageMaker including 
+      versions of Lorem Ipsum.`,
+      imgUrl: './assets/projectsImg/image.jpg',
+      buttonValue: 'In progress',
+      inProgress: true,
+    },
+  ];
 }
